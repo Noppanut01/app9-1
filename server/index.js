a0f0aa157cb19780cc14785cddff309e0e83e8dc
Noppanut <noppanut.jang2547@gmail.com>
2025-10-07 15:12:56 +0700

    add delete update

--- server/index.js
+++ server/index.js
@@ -3,46 +3,78 @@ const app = express()
 const Product = require('./model')
 const port = 8000
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.post('/api/db/create', (request, response) => {
     let form = request.body
     let data = {
-        name: form.name || '', 
-        price: form.price || 0, 
+        name: form.name || '',
+        price: form.price || 0,
         detail: form.detail || '',
         date_added: new Date(Date.parse(form.date_added)) || new Date()
     }
 
     Product.create(data, err => {
-        if (!err) { 
+        if (!err) {
             console.log('document saved')
-            response.send(true) 
+            response.send(true)
         } else {
             console.log(err)
-            response.send(false) 
-        }  
+            response.send(false)
+        }
     })
+})
+
+app.post('/api/db/update', (request, response) => {
+    let form = request.body
+    let data = {
+        name: form.name || '',
+        price: form.price || 0,
+        detail: form.detail || '',
+        date_added: new Date(Date.parse(form.date_added)) || new Date()
+    }
+    Product
+        .findByIdAndUpdate(form._id, data, { useFindAndModify: false })
+        .exec(err => {
+            if (err) {
+                response.json({ error: err })
+                return
+            }
+        })
+    Product.find().exec((err, docs) => {
+        response.json(docs)
+    })
+})
+app.post('/api/db/delete', (request, response) => {
+    let _id = request.body._id
+    Product
+        .findByIdAndDelete(_id, { useFindAndModify: false })
+        .exec(err => {
+            if (err) {
+                response.json({ error: err })
+                return
+            }
+        })
+    Product.find().exec((err, docs) => { response.json(docs) })
 })
 
 app.get('/api/db/read', (request, response) => {
     Product
-    .find()     
-    .exec((err, docs) => {
-        response.json(docs)
-    })
+        .find()
+        .exec((err, docs) => {
+            response.json(docs)
+        })
 })
 
 
-
 app.get('/api/db/paginate', (request, response) => {
-	let options = {
-		page: request.query.page || 1,     //เพจปัจจุบัน
-		limit: 2     //แสดงผลหน้าละ 2 รายการ (ข้อมูลมีน้อย)            
-	}
+    let options = {
+        page: request.query.page || 1,     //เพจปัจจุบัน
+        limit: 2     //แสดงผลหน้าละ 2 รายการ (ข้อมูลมีน้อย)            
+    }
 
-	Product.paginate({}, options, (err, result) => {
+    Product.paginate({}, options, (err, result) => {
         response.json(result)
     })
 })
@@ -51,25 +83,27 @@ app.get('/api/db/search', (request, response) => {
     let q = request.query.q || ''
 
     //กรณีนี้ให้กำหนด pattern ด้วย RegExp แทนการใช้ / /
-    let pattern = new RegExp(q, 'ig')   
+    let pattern = new RegExp(q, 'ig')
 
     //จะค้นหาจากฟิลด์ name และ detail
-    let conditions = {$or: [        
-                        {name: {$regex: pattern}}, 
-                        {detail: {$regex: pattern}}
-                     ]}	
+    let conditions = {
+        $or: [
+            { name: { $regex: pattern } },
+            { detail: { $regex: pattern } }
+        ]
+    }
 
     let options = {
-		page: request.query.page || 1,     //เพจปัจจุบัน
-		limit: 2     //แสดงผลหน้าละ 2 รายการ (ข้อมูลมีน้อย)               
-	}
+        page: request.query.page || 1,     //เพจปัจจุบัน
+        limit: 2     //แสดงผลหน้าละ 2 รายการ (ข้อมูลมีน้อย)               
+    }
 
-	Product
-	.paginate(conditions, options, (err, result) => {
-        response.json(result)
-    })
+    Product
+        .paginate(conditions, options, (err, result) => {
+            response.json(result)
+        })
 })
 
 app.listen(port, () => {
-	console.log('Server listening on port ' + port)
+    console.log('Server listening on port ' + port)
 })
